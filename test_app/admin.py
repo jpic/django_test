196a535802cd06c2f232f243dc155bdfa6f98b68
jpic <jamespic@gmail.com>
2012-11-21 15:23:02 +0100

    Implemented code and added data from question

--- test_app/admin.py
+++ test_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 from models import *
+
+
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = ('message', 'email')
+admin.site.register(Complaint, ComplaintAdmin)
+
+
+class ResponseLetterAdmin(admin.ModelAdmin):
+    list_display = ('date_response', 'response_from', 'complaint')
+admin.site.register(ResponseLetter)
--- test_app/models.py
+++ test_app/models.py
@@ -1,2 +1,27 @@
 from django.db import models
 
+
+class Complaint(models.Model):
+    message         = models.TextField(blank=True)
+    email           = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.email
+
+    def get_latest_response(self):
+        return ResponseLetter.objects.filter(complaint=self)[:1][0]
+
+
+class ResponseLetter(models.Model):
+    date_response       = models.DateField(db_index=True)
+    response_content    = models.TextField()
+    response_from       = models.CharField(max_length=255)
+
+    complaint           = models.ForeignKey(Complaint)
+
+    def __unicode__(self):
+        return self.response_from
+
+    class Meta:
+        ordering = ["-date_response"]
+
